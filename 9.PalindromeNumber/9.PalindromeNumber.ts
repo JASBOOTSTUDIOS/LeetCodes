680e5c3c29e49b6f2993f046c17345febae8e15b
Jose Luis Rojas Severino <jorojas@DCTIC.GC>
2025-02-06 00:31:27 -0400

    Add isPalindrome

---
+++ 9.PalindromeNumber/9.PalindromeNumber.ts
@@ -0,0 +1,10 @@
+function isPalindrome(x: number): boolean {
+    const y = x.toString();
+    let is_Palindrome = y.split("").reverse().join("") === x.toString()?  true : false; 
+    return is_Palindrome;
+};
+
+console.log(isPalindrome(123));
+console.log(isPalindrome(121));
+console.log(isPalindrome(-123));
+console.log(isPalindrome(11));
