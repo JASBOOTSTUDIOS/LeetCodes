f69a8a8c81eeca8d3cc1ce0bdaf31cf9756b7130
Jose Luis Rojas Severino <jorojas@DCTIC.GC>
2025-03-09 22:50:39 -0400

    Pendientes add

---
+++ index.js
@@ -0,0 +1,12 @@
+function map(arr, fn) {
+    var outArray = [];
+    for (var i = 0; i < arr.length; i++) {
+        outArray.push(fn(arr[i], i));
+    }
+    ;
+    return outArray;
+}
+;
+console.info(map([1, 2, 3], function plusone(n) { return n + 1; }));
+console.info(map([1, 2, 3], function plusI(n, i) { return n + i; }));
+console.info(map([10, 20, 30], function constante() { return 42; }));
